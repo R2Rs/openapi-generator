e8daaa0d0d65ed2e90983ec24356466f875eb353
William Cheng <wing328hk@gmail.com>
2020-05-05 15:17:00 +0800

    update powershell tests

--- samples/client/petstore/powershell-experimental/Test1.ps1
+++ samples/client/petstore/powershell-experimental/Test1.ps1
@@ -3,7 +3,7 @@
 Remove-Module -FullyQualifiedName @{ModuleName = "PSPetstore"; ModuleVersion = "0.1.2"}
 #Remove-Module -FullyQualifiedName @{ModuleName = "PSPetstore"; ModuleVersion = "0.0"}
 
-Import-Module -Name '.\src\PSPetstore\PSPetstore.psd1' -Verbose
+Import-Module -Name '.\src\PSPetstore\PSPetstore.psd1'
 #Import-Module -Name '.\src\PSPetstore\PSPetstore.psd1' -Verbose
 #Import-Module -Name '.\src\PSOpenAPITools'
 #Import-Module -Name '.\src\Org.OpenAPITools'
@@ -11,33 +11,30 @@ Import-Module -Name '.\src\PSPetstore\PSPetstore.psd1' -Verbose
 
 #$DebugPreference = 'Continue'
 
-$body = (Initialize-PSUser -Id 123  -Username "Username_example"  -FirstName "FirstName_example"  -LastName "LastName_example"  -Email "Email_example"  -Password "Password_example"  -Phone "Phone_example"  -UserStatus 123)
+$Id = 38360381209
+try {
+    $Result2 = Get-PSPetById -petId ($Id) 
+    Write-Host "No exception and result2 is $($Result2)"
+} catch {
+    if ($null -eq $Result2) {
+        Write-Host "Result2 is null: $($Result2)"
+    } else {
+        Write-Host "Result2 is not null: $($Result2)"
+    }
+}
 
-$Id = 38369
-
-#$result = Update-PSPetWithForm 
-#try {
-    $pet = Initialize-PSPet -Id $Id -Name 'foo' -Category (
-        Initialize-PSCategory -Id $Id -Name 'bar'
-    ) -PhotoUrls @(
-        'http://example.com/foo',
-        'http://example.com/bar'
-    ) -Tags (
-        Initialize-PSTag -Id 3 -Name 'baz'
-    ) -Status Available
-    
-    #Write-Host $pet
-    $Result = Add-PSPet -Pet $pet
-    Set-PSConfigurationApiKey -Id "api_key" -ApiKey "zzZZZZZZZZZZZZZ"
-    $Result2 = Get-PSPetById -petId ($Id) -Verbose -WithHttpInfo #-testHeader "testing only" -testQuery "testing something here"
-    Write-Host $Result2["Headers"]["Content-Type"]
-    $Result3 = Get-PSPetById -petId ($Id) -Verbose -WithHttpInfo -ReturnType "application/xml" #-testHeader "testing only" -testQuery "testing something here"
+try {
+    $Result3 = Get-PSPetById -petId ($Id) -WithHttpInfo
+    Write-Host "No exception and result3 is $($Result3)"
     Write-Host $Result3["Headers"]["Content-Type"]
     Write-Host $Result3["Response"]
-#} catch {
-#    Write-Host ("Exception occured when calling '': {0}" -f ($_.ErrorDetails | ConvertFrom-Json))
-#    Write-Host ("Response headers: {0}" -f ($_.Exception.Response.Headers | ConvertTo-Json))
-#}
+} catch {
+    if ($null -eq $Result3) {
+        Write-Host "Result3 is null: $($Result3)"
+    } else {
+        Write-Host "Result3 is not null: $($Result3)"
+    }
+}
 
 #$Result = Add-PSPet -Pet $pet -ReturnType "application/xml"
 #Write-Host "Before exit $($Result2.GetType())"
